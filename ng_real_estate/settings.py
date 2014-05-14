75128b1ec9aa6d444fa55c63a72e68d945e88eec
Geoff Boss <gboss4@gmail.com>
2014-05-13 19:26:39 -0700

    Setup initial file structure

--- ng_real_estate/settings.py
+++ ng_real_estate/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'real_estate_main',
 )
 
 MIDDLEWARE_CLASSES = (
--- ng_real_estate/urls.py
+++ ng_real_estate/urls.py
@@ -5,7 +5,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'ng_real_estate.views.home', name='home'),
+    url(r'^$', 'real_estate_main.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
