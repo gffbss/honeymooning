13571e4798fbe4afb5a9ba1e14bf91a26cfccde9
neal jones <alwaysworkharder@gmail.com>
2014-05-21 19:16:27 -0600

    changes

--- ng_real_estate/urls.py
+++ ng_real_estate/urls.py
@@ -1,12 +1,11 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'real_estate_main.views.home', name='home'),
-    url(r'^$', 'real_estate_main.views.about', name='about'),
+    url(r'^about/$', 'real_estate_main.views.about', name='about'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
--- real_estate_main/views.py
+++ real_estate_main/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def home(request):
-	return render(request, 'index.html')
+    return render(request, "index.html")
+
+
+def about(request):
+    return render(request, "about.html", {'about': about})
+
+
+def contact(request):
+    return render(request, "contact.html", {'contact': contact})
+
+
+def loans(request):
+    return render(request, "loans.html", {'loans': loans})
+
+
+def inventory(request):
+    return render(request, "inventory.html", {'inventory': inventory})
