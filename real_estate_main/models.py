0e11394612f5ce99f347cbaac40f4160098f7313
neal jones <alwaysworkharder@gmail.com>
2014-05-31 14:01:02 -0600

    messing with contact form, following a link on Trello, not working right now

--- real_estate_main/models.py
+++ real_estate_main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django import newforms as forms
+from django.newforms.widgets import *
+from django.core.mail import send_mail, BadHeaderError
 
 # Create your models here.
+
+class ContactForm(forms.Form):
+    name = forms.CharField()
+    email = forms.EmailField()
+    topic = forms.CharField()
+    message = forms.CharField(widget=Textarea())
