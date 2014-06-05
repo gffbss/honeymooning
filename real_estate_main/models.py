b64127892bb9da4fabba8134cae6735847f62c6b
neal jones <alwaysworkharder@gmail.com>
2014-06-04 18:35:41 -0600

    working on property page

--- real_estate_main/models.py
+++ real_estate_main/models.py
@@ -1,7 +1,34 @@
 from django.db import models
-
+from django.db.models import permalink
 
 # Create your models here.
+
+
+# class Homess(models.Model):
+#     title = models.CharField(max_length=100, unique=True)
+#     slug = models.SlugField(max_length=100, unique=True)
+#     body = models.TextField()
+#     posted = models.DateTimeField(db_index=True, auto_now_add=True)
+#     category = models.ForeignKey('homess.Category')
+#
+#     def __unicode__(self):
+#         return '%s' % self.title
+#
+#     @permalink
+#     def get_absolute_url(self):
+#         return ('view_homess_post', None, { 'slug': self.slug })
+#
+#
+# class Category(models.Model):
+#     title = models.CharField(max_length=100, db_index=True)
+#     slug = models.SlugField(max_length=100, db_index=True)
+#
+#     def __unicode__(self):
+#         return '%s' % self.title
+#
+#     @permalink
+#     def get_absolute_url(self):
+#         return ('view_homess_category', None, { 'slug': self.slug })
 
 
 # class ContactForm(models.Model):
