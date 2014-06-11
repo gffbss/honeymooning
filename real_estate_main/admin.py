e0124571e540e560f9bbd8619c5d4bd71211762e
neal jones <alwaysworkharder@gmail.com>
2014-06-10 18:41:48 -0600

    working on front page aesthetics

--- real_estate_main/admin.py
+++ real_estate_main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from honeymooning.real_estate_main.models import Homess, Category
+# from honeymooning.real_estate_main.models import Homess, Category
 
 # Register your models here.
 
@@ -8,13 +8,13 @@ from honeymooning.real_estate_main.models import Homess, Category
 # from blog.models import Blog, Category
 
 
-class HomessAdmin(admin.ModelAdmin):
-    exclude = ['posted']
-    prepopulated_fields = {'slug': ('title',)}
-
-class CategoryAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug': ('title',)}
-
-
-admin.site.register(Homess)
-admin.site.register(Category)
+# class HomessAdmin(admin.ModelAdmin):
+#     exclude = ['posted']
+#     prepopulated_fields = {'slug': ('title',)}
+#
+# class CategoryAdmin(admin.ModelAdmin):
+#     prepopulated_fields = {'slug': ('title',)}
+#
+#
+# admin.site.register(Homess)
+# admin.site.register(Category)
